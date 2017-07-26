72339dbf995b7a5534a5551fdeff5dbf18238882
amit19-meet <amit19@meet.mit.edu>
2017-07-26 16:58:19 +0300

    Stopping here

---
+++ turtle_party.py
@@ -0,0 +1,16 @@
+strawberries = 50
+is_weekend = True
+
+
+if is_weekend:
+    if strawberries > 39:
+        print("Fun")
+    else:
+        print("Not fun")
+
+else: # weekday
+    if strawberries > 39 and strawberries < 61:
+        print("Fun")
+    else:
+        print("Not fun")
+        
